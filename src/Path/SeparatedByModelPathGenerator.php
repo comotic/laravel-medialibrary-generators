7977fdfff16de2778a75e38dc7df810a08d4ae86
Nicolas Perraut <n.perraut@gmail.com>
2021-05-07 19:26:24 +0200

    Add UuidPathGenerator and SeparateByModelPathGenerator

---
+++ src/Path/SeparatedByModelPathGenerator.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Comotic\LaravelMedialibraryGenerators\Path;
+
+
+use Illuminate\Support\Str;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\MediaLibrary\Support\PathGenerator\DefaultPathGenerator;
+
+class SeparatedByModelPathGenerator extends DefaultPathGenerator
+{
+    public function getBasePath(Media $media): string
+    {
+        $modelType = Str::of($media->getMorphClass())
+                        ->explode("\\")
+                        ->last();
+
+        return "$modelType/$media->model_id/{$media->getKey()}";
+    }
+
+}
---
+++ src/Path/UuidPathGenerator.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Comotic\LaravelMediaLibraryGenerators\Path;
+
+
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\MediaLibrary\Support\PathGenerator\DefaultPathGenerator;
+
+class UuidPathGenerator extends DefaultPathGenerator
+{
+    public function getBasePath(Media $media): string
+    {
+        return $media->getAttribute('uuid');
+    }
+
+}
